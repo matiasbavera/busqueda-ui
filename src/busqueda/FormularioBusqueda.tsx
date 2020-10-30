a540b96b04b69aaba9d72d81690b23bcd3a49c22
Matias Bavera <matiasbavera@gmail.com>
2020-10-29 23:45:03 -0300

    Added axios

--- src/busqueda/FormularioBusqueda.tsx
+++ src/busqueda/FormularioBusqueda.tsx
@@ -1,20 +1,36 @@
 import { IconButton, TextField } from '@material-ui/core';
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
+import axios from 'axios';
 
-export const FormularioBusqueda =(props:any) =>{
-    return (
-        <>
-          <TextField label={'CI'}></TextField>
-          <TextField label={'Nombres'}></TextField>
-          <TextField label={'Apellidos'}></TextField>
-          {/* <TextField label={'Caso'}></TextField>
-          <TextField label={'Despacho'}></TextField>
-          <TextField label={'ID. Documento'}></TextField> */}
-          <IconButton id="buscar-btn" color="primary">
-            <SearchIcon />
-          </IconButton>
-        </>
-    )
-}
+axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+axios.defaults.xsrfCookieName = 'csrftoken';
+export const FormularioBusqueda = (props: any) => {
+  const handleFormRequest = (): void => {
+    console.log('entre');
 
+    axios
+      .post('http://localhost:8000/persona/buscar/', {
+        nombre_apellido: 'hola',
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  return (
+    <form>
+      <TextField label={'CI'} variant="outlined"></TextField>
+      <TextField label={'Nombres'} variant="outlined"></TextField>
+      <TextField label={'Apellidos'} variant="outlined"></TextField>
+      {/* <TextField label={'Caso'}></TextField>
+            <TextField label={'Despacho'}></TextField>
+            <TextField label={'ID. Documento'}></TextField> */}
+      <IconButton id="buscar-btn" color="primary" onClick={handleFormRequest}>
+        <SearchIcon />
+      </IconButton>
+    </form>
+  );
+};
