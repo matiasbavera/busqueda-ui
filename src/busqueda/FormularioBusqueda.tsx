c405f10848afb699c84e9de4fbd3062932c82b19
Matias Bavera <matiasbavera@gmail.com>
2020-11-05 23:10:27 -0300

    zxios added

--- src/busqueda/FormularioBusqueda.tsx
+++ src/busqueda/FormularioBusqueda.tsx
@@ -3,16 +3,26 @@ import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import axios from 'axios';
 
+const apiUrl = 'http://0.0.0.0:8000/persona/buscar/';
+let crf_token = $("[name=csrfmiddlewaretoken]").val();
+console.log('token ', crf_token)
+
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 axios.defaults.xsrfCookieName = 'csrftoken';
+
 export const FormularioBusqueda = (props: any) => {
   const handleFormRequest = (): void => {
     console.log('entre');
 
-    axios
-      .post('http://localhost:8000/persona/buscar/', {
-        nombre_apellido: 'hola',
-      })
+    axios({
+      method: 'post',
+      url: `${apiUrl}`,
+      headers: {
+          'Content-Type': 'application/json',
+          "X-CSRFToken": crf_token
+      },
+      data: {nombre_apellido: 'hola'}
+    })
       .then(function (response) {
         console.log(response);
       })
