9d9f727443a91b1870371b6ef77059d5559aca27
Matias Bavera <matiasbavera@gmail.com>
2020-11-06 00:19:51 -0300

    Added csrftoken validation

--- src/busqueda/FormularioBusqueda.tsx
+++ src/busqueda/FormularioBusqueda.tsx
@@ -3,12 +3,25 @@ import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import axios from 'axios';
 
-const apiUrl = 'http://0.0.0.0:8000/persona/buscar/';
-let crf_token = $("[name=csrfmiddlewaretoken]").val();
-console.log('token ', crf_token)
+const apiUrl = 'http://localhost:8000/persona/buscar/';
 
-axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
-axios.defaults.xsrfCookieName = 'csrftoken';
+function getCookie(name:any) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+console.log('token ', csrftoken)
 
 export const FormularioBusqueda = (props: any) => {
   const handleFormRequest = (): void => {
@@ -19,7 +32,7 @@ export const FormularioBusqueda = (props: any) => {
       url: `${apiUrl}`,
       headers: {
           'Content-Type': 'application/json',
-          "X-CSRFToken": crf_token
+          "X-CSRFToken": csrftoken
       },
       data: {nombre_apellido: 'hola'}
     })
