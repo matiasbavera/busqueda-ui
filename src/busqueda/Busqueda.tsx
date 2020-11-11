7a5ddf0bce178c77cfa35c3b2a5a40e46aa0f2d0
Matias Bavera <matiasbavera@gmail.com>
2020-11-10 22:47:47 -0300

    Moviendo la lógica de post a componente padre

--- src/busqueda/Busqueda.tsx
+++ src/busqueda/Busqueda.tsx
@@ -2,6 +2,27 @@ import { createStyles, Divider, makeStyles, Theme } from '@material-ui/core';
 import React from 'react';
 import { FormularioBusqueda } from './FormularioBusqueda';
 import { Data, TablaBusqueda } from './TablaBusqueda';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:8000/persona/buscar/';
+
+function getCookie(name:any) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
 
 function createData(documento: string, nombre: string, apellido: string): Data {
   return { documento, nombre, apellido };
@@ -10,14 +31,35 @@ function createData(documento: string, nombre: string, apellido: string): Data {
 export const Busqueda = (props: any) => {
   const classes = useStyles();
   const lista = [createData('1', 'mati', 'b'), createData('2', 'john', 'doe')];
+  const [listaMatch, setListaMatch] = React.useState([] as Data[])
+  
+  const handleFormRequest = (): void => {
+    axios({
+      method: 'post',
+      url: `${apiUrl}`,
+      headers: {
+          'Content-Type': 'application/json',
+          "X-CSRFToken": csrftoken
+      },
+      data: {nombre_apellido: 'hola'}
+    })
+      .then(function (response) {
+        setListaMatch(response.data)
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className={classes.formulario}>
-        <FormularioBusqueda></FormularioBusqueda>
+        <FormularioBusqueda handleFormRequest={handleFormRequest}></FormularioBusqueda>
       </div>
       <Divider></Divider>
       <div className={classes.formulario}>
-        <TablaBusqueda datos={lista}></TablaBusqueda>
+        <TablaBusqueda datos={listaMatch}></TablaBusqueda>
       </div>
     </>
   );
--- src/busqueda/FormularioBusqueda.tsx
+++ src/busqueda/FormularioBusqueda.tsx
@@ -1,51 +1,32 @@
 import { IconButton, TextField } from '@material-ui/core';
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import axios from 'axios';
 
-const apiUrl = 'http://localhost:8000/persona/buscar/';
-
-function getCookie(name:any) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
+interface FormularioBusquedaProps {
+  handleFormRequest():void;
 }
-const csrftoken = getCookie('csrftoken');
-console.log('token ', csrftoken)
 
-export const FormularioBusqueda = (props: any) => {
-  const handleFormRequest = (): void => {
-    console.log('entre');
-
-    axios({
-      method: 'post',
-      url: `${apiUrl}`,
-      headers: {
-          'Content-Type': 'application/json',
-          "X-CSRFToken": csrftoken
-      },
-      data: {nombre_apellido: 'hola'}
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+export const FormularioBusqueda = (props: FormularioBusquedaProps) => {
+  const {handleFormRequest}=props;
+  
   return (
     <form>
-      <TextField label={'CI'} variant="outlined"></TextField>
+       <TextField
+          id="ci"
+          name="ci"
+          onChange={(e) => {
+            setCi(!!e.target.value ? parseInt(e.target.value) : 0);
+          }}
+          placeholder="Numero de documento"
+          type="string"
+          value={ci || ''}
+          // className={classes.input}
+          label="Numero de documento"
+          variant="outlined"
+          error={!!ciError}
+          helperText={ciError}
+        />
+      {/* <TextField label={'CI'} variant="outlined"></TextField> */}
       <TextField label={'Nombres'} variant="outlined"></TextField>
       <TextField label={'Apellidos'} variant="outlined"></TextField>
       {/* <TextField label={'Caso'}></TextField>
