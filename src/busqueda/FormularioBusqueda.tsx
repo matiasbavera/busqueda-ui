01a9febd8660afffe25005f9b7652ec7740246e1
Matias Bavera <matiasbavera@gmail.com>
2020-11-23 10:41:35 -0300

    Se agregó un formulario con manejo de estados

--- src/busqueda/FormularioBusqueda.tsx
+++ src/busqueda/FormularioBusqueda.tsx
@@ -8,14 +8,37 @@ interface FormularioBusquedaProps {
 
 export const FormularioBusqueda = (props: FormularioBusquedaProps) => {
   const {handleFormRequest}=props;
+  const [ci, setCi] = React.useState('');
+  const [nombre, setNombre] = React.useState('');
+  const [apellido, setApellido] = React.useState('');
   
+  const [errorCi, setErrorCi] =  React.useState('');
+  const [errorNombre, setErrorNombre] =  React.useState('');
+  const [errorApellido, setErrorApellido] =  React.useState('');
+
+
+  const isFormValid = (): boolean =>{
+    let isValid = true;
+    if (ci === '' && nombre === '' && apellido === ''){
+      isValid = false;
+    }
+    return isValid
+  }
+
+  const formRequest = ():void =>{
+    if (isFormValid()){
+      handleFormRequest();
+    }
+
+  }
+
   return (
     <form>
        <TextField
           id="ci"
           name="ci"
           onChange={(e) => {
-            setCi(!!e.target.value ? parseInt(e.target.value) : 0);
+            setCi(e.target.value);
           }}
           placeholder="Numero de documento"
           type="string"
@@ -23,12 +46,41 @@ export const FormularioBusqueda = (props: FormularioBusquedaProps) => {
           // className={classes.input}
           label="Numero de documento"
           variant="outlined"
-          error={!!ciError}
-          helperText={ciError}
+          error={!!errorCi}
+          helperText={errorCi}
         />
-      {/* <TextField label={'CI'} variant="outlined"></TextField> */}
-      <TextField label={'Nombres'} variant="outlined"></TextField>
-      <TextField label={'Apellidos'} variant="outlined"></TextField>
+
+        <TextField
+          id="nombre"
+          name="nombre"
+          onChange={(e) => {
+            setNombre(e.target.value);
+          }}
+          placeholder="Nombre"
+          type="string"
+          value={nombre || ''}
+          // className={classes.input}
+          label="Nombre"
+          variant="outlined"
+          error={!!errorNombre}
+          helperText={errorNombre}
+        />
+
+        <TextField
+          id="apellido"
+          name="apellido"
+          onChange={(e) => {
+            setApellido(e.target.value);
+          }}
+          placeholder="Apellido"
+          type="string"
+          value={apellido || ''}
+          // className={classes.input}
+          label="Apellido"
+          variant="outlined"
+          error={!!errorApellido}
+          helperText={errorApellido}
+        />
       {/* <TextField label={'Caso'}></TextField>
             <TextField label={'Despacho'}></TextField>
             <TextField label={'ID. Documento'}></TextField> */}
